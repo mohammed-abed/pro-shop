4bbf4281b573c8c09a687ba60e1f753826d3070c
mohammedabed <37789022+mohammedabed@users.noreply.github.com>
2021-06-13 23:08:33 +0300

    update1

---
+++ src/App.Styled.js
@@ -0,0 +1,69 @@
+
+import styled from "styled-components";
+export const Main=styled("Main")`
+
+max-width: 1920px;
+`
+
+export const MainContainer=styled("MainContainer")`
+
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  background: white;
+  flex-direction: row;
+`
+export const Nav=styled("Nav")`
+height:100%;
+`
+export const NavBar=styled("NavBar")`
+display: flex;
+flex-direction: row;
+justify-content:space-between;
+align-items:center;
+background:black;
+height:85px;
+
+`
+export const ProfileCard=styled("ProfileCard")`
+
+display: flex;
+flex-direction: column;
+justify-content:center;
+align-items:center;
+height: 100%;
+background:#F2F2F2;
+width: 398px;
+height: 528px;
+
+`
+export const ProfileDetail=styled("ProfileDetail")`
+
+display: flex;
+flex-direction: row;
+justify-content:start;
+align-items:start;
+height: 100%;
+background:#F2F2F2;
+width: 950px;
+height: 528px;
+
+`
+export const Typography = styled("p")`
+  font-size: ${(props) => props.fontSize}px;
+  color: ${(props) => props.color};
+`
+export const Button = styled("button")`
+ 
+  height: 40px;
+  border: none;
+  font-size: ${(props) => props.fontSize}px;
+  background: ${(props) => props.background};
+  font-weight: ${(props) => props.fontWeight}px;
+  cursor: pointer;
+  `
+;
+
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import {MainContainer,NavBar,ProfileCard,ProfileDetail,Typography,Button} from"./App.Styled.js"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <main>
+    <NavBar>
 
+        <div>
+        <img src=" " alt="Logo" >
+          </img>
+        </div>
+        <div>
+          <input type="text">
+
+
+          </input>
+          <Button fontSize={16} background="#FCDD06">Search</Button>
+
+        </div>
+        <div>
+        <img src=" " alt="icon1" >
+          </img>
+        <img src=" " alt="icon2" >
+          </img>
+        <img src=" " alt="icon3" >
+          </img>
+          </div>
+
+      </NavBar>
+      <MainContainer>
+      <ProfileCard>
+        <Typography fontSize={32}>
+          <img src=" " alt=" " >
+          </img>
+          Amy Mayer </Typography>
+        <Typography fontSize={24}>My Orders</Typography>
+        <Typography fontSize={24}>Wishlist</Typography>
+        <Typography  fontSize={24}>Notifcations</Typography>
+        <Typography  fontSize={24}>Settings</Typography>
+        <Button  fontSize={24} fontWeight={300}>Logout</Button>
+
+
+            </ProfileCard>
+            <ProfileDetail>
+              <div>
+            <Typography fontSize={32}>My Profile </Typography>
+        <Typography fontSize={24}>First Name </Typography>
+        <Typography fontSize={24}>Last Name</Typography>
+        <Typography Typography fontSize={24}>Last Name</Typography>
+        <Typography Typography fontSize={24}>Birthday</Typography>
+        <Typography Typography fontSize={24}>Logout</Typography>
+        <Button fontSize={13} background="#FCDD06">Change Password</Button>
+        </div>
+        <div>
+         <Typography fontSize={32}>My Profile </Typography>
+        <Typography fontSize={24}>Amy</Typography>
+        <Typography fontSize={24}>Mayer</Typography>
+        <Typography Typography fontSize={24}>amymay@gmail.com</Typography>
+        <Typography Typography fontSize={24}>12/04/1991</Typography>
+        <Typography Typography fontSize={24}>Logout</Typography>
+        </div>
+        <div>
+        <img src=" " alt=" " >
+          </img>
+          <Button fontSize={13} background="#FCDD06">Upload new photo</Button>
+
+        </div>
+            </ProfileDetail>
+
+          </MainContainer>
+          </main>
+  );
+};
 export default App;
